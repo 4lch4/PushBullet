da80a1512880f571be70a876afc46bcd685c7a4f
4lch4 <git@4lch4.dev>
2021-07-05 09:54:25 -0500

    fix: added endpoints to root file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,51 @@
+import {
+  ChannelEndpoint,
+  ChatEndpoint,
+  DeviceEndpoint,
+  PushEndpoint,
+  SubscriptionsEndpoint,
+  TextEndpoint,
+  UploadEndpoint,
+  UserEndpoint
+} from './endpoints'
 import { APIConfig } from './interfaces/APIConfig'
 
 export class PBLib {
-  config: APIConfig
+  private config: APIConfig
 
   constructor(config: APIConfig) {
     this.config = config
+  }
+
+  get ChannelEndpoint() {
+    return new ChannelEndpoint(this.config)
+  }
+
+  get ChatEndpoint() {
+    return new ChatEndpoint(this.config)
+  }
+
+  get DeviceEndpoint() {
+    return new DeviceEndpoint(this.config)
+  }
+
+  get PushEndpoint() {
+    return new PushEndpoint(this.config)
+  }
+
+  get SubscriptionsEndpoint() {
+    return new SubscriptionsEndpoint(this.config)
+  }
+
+  get TextEndpoint() {
+    return new TextEndpoint(this.config)
+  }
+
+  get UploadEndpoint() {
+    return new UploadEndpoint(this.config)
+  }
+
+  get UserEndpoint() {
+    return new UserEndpoint(this.config)
   }
 }
